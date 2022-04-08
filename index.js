63138bb45569cb4feeceeb39dc62b7387bfbd004
shpande <103243192+shpande@users.noreply.github.com>
2022-04-08 12:48:46 +0530

    added index.js and buildspec.yml

---
+++ index.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+
+exports.handler = async (event, context, callback) => {
+    const requestId = context.awsRequestId;
+    await createMsg(requestId, event).then(()=> {
+        callback(null, {
+            statusCode : 201,
+            body : '',
+            headers : {
+                'Access-Control-Allow-Origin' : '*'
+            }
+        })
+   }).catch((err)=>{
+        console.error(err)
+    });
+
+};
+
+function createMsg(requestId, event)
+{
+    const param = {
+        TableName : 'Message',
+        Item : {
+            'messageId' : requestId,
+            'message' : event
+        }
+    }
+    return ddb.put(param).promise();
+}
